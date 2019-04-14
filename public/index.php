58120bc8b23663cf11bd2d7de07107a635619591
Anton Makieiev <makeey97@gmail.com>
2019-04-15 01:07:07 +0300

    Move public and views

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use KanbanBoard\Infrastructure\ApplicationInterface;
 
-$container = require_once '../../bootstrap.php';
+$container = require_once '../bootstrap.php';
 
 $container->call(static function(ApplicationInterface $application){
    $application->run();
