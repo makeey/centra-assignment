5bfd5d1a0e38fa35a55ce654d78c7fdb18a58853
Anton Makieiev <makeey97@gmail.com>
2019-04-15 21:40:09 +0300

    Optional load .env

--- bootstrap.php
+++ bootstrap.php
@@ -7,13 +7,19 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
+
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
-$builder = new DI\ContainerBuilder();
-$builder->useAutowiring(true);
-$builder->addDefinitions(require_once __DIR__. '/config/di.php');
-$container = $builder->build();
-
-return $container;
+if (! isset($GLOBALS['container'])) {
+    if (\file_exists(__DIR__ . '/.env')) {
+        $dotenv = Dotenv::create(__DIR__, '.env');
+        $dotenv->load();
+    }
+    $builder = new DI\ContainerBuilder();
+    $builder->useAutowiring(true);
+    $builder->addDefinitions(require_once __DIR__ . '/config/di.php');
+    $GLOBALS['container'] = $builder->build();
+}
+return $GLOBALS['container'];
